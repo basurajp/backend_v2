6276bae444773b0eecc3a62082de9311967a1ae6
Basuraj <basurajp135@gmail.com>
2024-02-10 10:15:56 +0530

    middle ware setup done , express.json , urlencoded , static files , cookiePareser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import dotenv from "dotenv";
 import connnetDB from "./db/db.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connnetDB();
+connnetDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running as port ${process.env.PORT}`);
+    });
+  })
+
+  
+  .catch((err) => {
+    console.log("Mongo Db connection failed !!! ", err);
+  });
